06aeb66e7b6247f7c365863208dd24a04787cfff
Eduardo Salvador Reinheimer <du428@hotmail.com>
2021-10-12 20:23:02 -0300

    Primeiro commit

---
+++ src/ex01.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ex01 {
+	static final String DEFAULT_INPUT_MESSAGE = "Digite uma nota entre 0 e 10: ";
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		System.out.print(DEFAULT_INPUT_MESSAGE);
+		float nota = input.nextFloat();
+
+		while (nota < 0 || nota > 10) {
+			System.out.print(DEFAULT_INPUT_MESSAGE);
+			nota = input.nextFloat();
+		}
+
+	}
+
+}
